92140fec5d5e1ffa3b6bfe54aef9fb17a2ac58da
Ashish kumar <av314880@gmail.com>
2024-07-23 17:56:57 +0530

    first commit

---
+++ src/Components/navbar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <>
+            <header>
+                <nav className='flex  ' >
+                    <div>Logo</div>
+                    <div>
+                        <ul>
+                            <li>Home</li>
+                            <li>About</li>
+                            <li>pass</li>
+                            <li>fail</li>
+                        </ul>
+                    </div>
+                </nav>
+
+            </header>
+        </>
+    )
+}
+
+export default Navbar;
